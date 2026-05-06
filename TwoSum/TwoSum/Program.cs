76319169b09b745ed2fb5c6725b3b59083002121
unknown <udayarai@hotmail.co.uk>
2026-05-06 23:02:27 +0100

    Twosum intial solution added

---
+++ TwoSum/TwoSum/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var program = new Program();
+            program.TwoSum([2, 7, 11, 15], 9);
+        }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            // int[] result = new int[2]{0, 0};
+            // int currentIndex = 0;
+            // foreach (int num in nums)
+            // {
+            //     int difference = target - num;
+
+            //     var index = Array.IndexOf(nums, difference);
+
+            //     if (index != -1 && index != currentIndex)
+            //     {
+            //         result[0] = currentIndex;
+            //         result[1] = index;
+            //         break;
+            //     }
+            //     currentIndex++;
+            // }
+            // return result;
+
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            int[] result = new int[2];
+
+            int index = 0;
+
+            foreach (int num in nums)
+            {
+                int compliment = target - num;
+                bool containsCompliment = dict.TryGetValue(compliment, out int value);
+                Console.WriteLine("run" + index);
+                Console.WriteLine(containsCompliment);
+                Console.WriteLine(value);
+                Console.WriteLine("\n");
+                if (containsCompliment)
+                {
+                    result[0] = value;
+                    result[1] = index;
+                    break;
+                }
+                dict.Add(num, index);
+                index++;
+            }
+            return result;
+        }
+    }
+
+
+}
