422bf6f6f413cda8fa1c53363dddd407f474966d
unknown <udayarai@hotmail.co.uk>
2026-05-09 07:40:57 +0100

    update to only add unique key to dictionary

--- TwoSum/TwoSum/Program.cs
+++ TwoSum/TwoSum/Program.cs
@@ -1,4 +1,20 @@
-﻿namespace TwoSum
+﻿/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ 
+ */
+
+
+namespace TwoSum
 {
     internal class Program
     {
@@ -49,7 +65,14 @@
                     result[1] = index;
                     break;
                 }
-                dict.Add(num, index);
+
+                //check for any possible duplicate keys
+                //if (!dict.ContainsKey(num))
+                //{
+                //    dict.Add(num, index);
+                //}
+                dict.TryAdd(num, index);
+
                 index++;
             }
             return result;
