7b4a7bb6a780a791b7493a1b06e2b696ba796b89
unknown <udayarai@hotmail.co.uk>
2026-05-09 11:23:26 +0100

    initial solution

---
+++ ValidAnagram/ValidAnagram/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ValidAnagram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Console.WriteLine(program.IsAnagram("racecar", "carrace"));
+            
+        }
+
+        public bool IsAnagram(string s, string t)
+        {
+            //first check the length must be equal
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            //convert strings into char arrays
+            char[] sArr = s.ToCharArray();
+            char[] tArr = t.ToCharArray();
+
+            //sort out the arrays
+            Array.Sort(sArr);
+            Array.Sort(tArr);
+
+            //compare the array contents 
+            return sArr.SequenceEqual(tArr);
+        }
+    }
+}
