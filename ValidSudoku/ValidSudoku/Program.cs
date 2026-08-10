0b790bcb555f57a29fe83937b273fc5482643896
unknown <udayarai@hotmail.co.uk>
2026-08-10 21:32:25 +0100

    refactor: add refactor to remove individual traversal for rows, columns and subbox

--- ValidSudoku/ValidSudoku/Program.cs
+++ ValidSudoku/ValidSudoku/Program.cs
@@ -33,66 +33,22 @@ namespace ValidSudoku
                 ['.','.','.','4','1','9','.','.','8'],
                 ['.','.','.','.','8','.','.','7','9']
             ];
-            Console.WriteLine(IsValidSudoku(board));
+
+            char[][] board2 = [
+                ['7', '.', '.', '.', '4', '.', '.', '.', '.'],
+                ['.', '.', '.', '8', '6', '5', '.', '.', '.'],
+                ['.', '1', '.', '2', '.', '.', '.', '.', '.'],
+                ['.', '.', '.', '.', '.', '9', '.', '.', '.'],
+                ['.', '.', '.', '.', '5', '.', '5', '.', '.'],
+                ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                ['.', '.', '.', '.', '.', '.', '2', '.', '.'],
+                ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                ['.', '.', '.', '.', '.', '.', '.', '.', '.']];
+            Console.WriteLine(IsValidSudoku(board2));
         }
 
         public static bool IsValidSudoku(char[][] board)
         {
-            //columns
-            for (int i = 0; i < board.Length; i++)
-            {
-                HashSet<char> columns = new HashSet<char>();
-                for (int j = 0; j < board.Length; j++)
-                {
-                    char columnElement = board[j][i];
-                    if (columns.Contains(columnElement))
-                    {
-                        return false;
-                    }
-
-                    if (columnElement == '.') 
-                    {
-                        continue;
-                    } else if (Char.GetNumericValue(columnElement) > 0 && Char.GetNumericValue(columnElement) < 10)
-                    {
-                        columns.Add(columnElement);
-                    } else
-                    {
-                        return false;
-                    }
-
-                }
-            }
-
-            //rows
-            
-            for (int i = 0; i < board.Length; i++)
-            {
-                HashSet<char> rows = new HashSet<char>();
-                for (int j = 0; j < board.Length; j++)
-                {
-                    char rowElement = board[i][j];
-                    if (rows.Contains(rowElement))
-                    {
-                        return false;
-                    }
-
-                    if (rowElement == '.')
-                    {
-                        continue;
-                    } else if (Char.GetNumericValue(rowElement) > 0 && Char.GetNumericValue(rowElement) < 10) 
-                    {
-                        rows.Add(rowElement);
-                    } else
-                    {
-                        return false;
-                    }
-                }
-                
-            }
-
-
-
             //3x3 subBox
             int noOfSubBox = (board.Length * board.Length) / 9; //9 - 3x3 subboxes
             HashSet<char>[] subBoxArray = new HashSet<char>[noOfSubBox];
@@ -102,13 +58,57 @@ namespace ValidSudoku
                 subBoxArray[i] = new HashSet<char>();
             }
 
+
             for (int i = 0; i < board.Length; i++)
             {
+                HashSet<char> columns = new HashSet<char>();
+                HashSet<char> rows = new HashSet<char>();
+
                 for (int j = 0; j < board.Length; j++)
                 {
+                    //columns
+                    char columnElement = board[j][i];
+                    if (columns.Contains(columnElement))
+                    {
+                        return false;
+                    }
+
+                    if (columnElement == '.') 
+                    {
+                        //continue;
+                    } else if (Char.GetNumericValue(columnElement) > 0 && Char.GetNumericValue(columnElement) < 10)
+                    {
+                        columns.Add(columnElement);
+                    } else
+                    {
+                        return false;
+                    }
+
+
+                    //row
+                    char rowElement = board[i][j];
+                    if (rows.Contains(rowElement))
+                    {
+                        return false;
+                    }
+
+                    if (rowElement == '.')
+                    {
+                        //continue;
+                    }
+                    else if (Char.GetNumericValue(rowElement) > 0 && Char.GetNumericValue(rowElement) < 10)
+                    {
+                        rows.Add(rowElement);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                    //3x3 subbox
                     char item = board[i][j];
-                    int subBoxIndex = i/3 * 3 + j/3;
-                    
+                    int subBoxIndex = i / 3 * 3 + j / 3;
+
                     if (subBoxArray[subBoxIndex].Contains(item))
                     {
                         return false;
@@ -117,14 +117,17 @@ namespace ValidSudoku
                     //add what we have already seen
                     if (item == '.')
                     {
-                        continue;
-                    }  else if (Char.GetNumericValue(item) > 0 && Char.GetNumericValue(item) < 10)
+                        //continue;
+                    }
+                    else if (Char.GetNumericValue(item) > 0 && Char.GetNumericValue(item) < 10)
                     {
                         subBoxArray[subBoxIndex].Add(item);
-                    }  else
+                    }
+                    else
                     {
                         return false;
                     }
+
                 }
             }
             
